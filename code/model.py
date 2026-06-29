9bc983684393e72e20fc1ab1e4bd1915ddca0979
Shashikant Kumar <shashikantstm22@gmail.com>
2026-06-29 14:47:00 +0530

    Update model.py

--- code/model.py
+++ code/model.py
@@ -1,1 +1,21 @@
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import pandas as pd
 
+class CropRecommendationModel:
+    def _init_(self):
+        self.model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    def train(self, csv_file):
+        data = pd.read_csv(csv_file)
+
+        X = data[["temperature", "humidity", "soil_moisture"]]
+        y = data["pump_status"]
+
+        self.model.fit(X, y)
+        joblib.dump(self.model, "crop_model.pkl")
+        print("Model trained successfully!")
+
+    def predict(self, temperature, humidity, soil_moisture):
+        prediction = self.model.predict([[temperature, humidity, soil_moisture]])
+        return prediction[0]
