a1ae66d5e6817bdec5dbbb80667d34dac9431ce6
Shashikant Kumar <shashikantstm22@gmail.com>
2026-06-29 15:05:46 +0530

    Update dashboard.py

--- code/dashboard.py
+++ code/dashboard.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+from sensor import Sensor
 
+app = Flask(_name_)
+sensor = Sensor()
+
+@app.route("/")
+def dashboard():
+    data = sensor.get_sensor_data()
+    return render_template("dashboard.html", data=data)
+
+if _name_ == "_main_":
+    app.run(debug=True)
