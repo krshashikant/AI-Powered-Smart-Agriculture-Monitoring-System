09ee8bc4402422cf66ab153f8f8ab8aa8aabed56
Shashikant Kumar <shashikantstm22@gmail.com>
2026-06-29 15:06:59 +0530

    Update train_model.py

--- code/train_model.py
+++ code/train_model.py
@@ -1,1 +1,15 @@
+from model import CropRecommendationModel
 
+def main():
+    model = CropRecommendationModel()
+
+    # Dataset path
+    csv_file = "../dataset/crop_data.csv"
+
+    # Train model
+    model.train(csv_file)
+
+    print("Training Completed Successfully!")
+
+if _name_ == "_main_":
+    main()
